9b9c5dde4f0ef9da44fe547da1750e63c7de66fa
Christian Grothaus <cgrothaus@outlook.com>
2022-10-01 23:21:39 -0400

    Create HTTP controller to use for all HTML document needs

---
+++ TwitterFaker/Controllers/HTTPController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using TwitterFaker.Models;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace TwitterFaker.Controllers
+{
+    public class HTTPController : Controller
+    {
+        //TODO add all functionality
+        public async void GetPage()
+        {
+            HttpClient client = new HttpClient();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(await client.GetStreamAsync("https://twitter.com/elonmusk"));
+        }
+    }
+}
