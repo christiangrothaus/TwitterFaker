86e148dc80e4375e6c1317489972e381b6b727e8
Christian Grothaus <cgrothaus@outlook.com>
2022-10-02 21:38:43 -0400

    Create UserController.cs and edit layout to utilize it

---
+++ TwitterFaker/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using TwitterFaker.Models;
+
+namespace TwitterFaker.Controllers
+{
+    public class UserController : Controller
+    {
+        private HttpContext _httpContext;
+
+        public void setContext(HttpContext httpContext)
+        {
+            this._httpContext = httpContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public string GetCookie(string key)
+        {
+          return _httpContext.Request.Cookies[key];
+        }
+
+        public void SetCookie(string key, string value, int? expireTime)
+        {
+            CookieOptions option = new CookieOptions();
+            option.IsEssential = true;
+
+            if (expireTime.HasValue)
+                option.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+            else
+                option.Expires = DateTime.Now.AddYears(1);
+
+            _httpContext.Response.Cookies.Append(key, value, option);
+        }
+
+        public void RemoveCookie(string key)
+        {
+            _httpContext.Response.Cookies.Delete(key);
+        }
+    }
+}
