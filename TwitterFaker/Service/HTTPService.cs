4e353f6d578c996f802ae77aa80161ce8dc03865
Christian Grothaus <cgrothaus@outlook.com>
2022-10-02 21:35:39 -0400

    Refactor HTTPController.cs to HTTPService.cs

---
+++ TwitterFaker/Service/HTTPService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace TwitterFaker.Service
+{
+    public class HTTPService
+    {
+        public async Task<string> GetName(string account)
+        {
+            HttpClient client = new HttpClient();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(await client.GetStreamAsync($"https://twitter.com/{account}"));
+            string name = doc.DocumentNode.SelectSingleNode("//*[@id=\"react-root\"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div[1]/div/div[1]/div/div/span[1]/span").InnerText;
+            return name;
+        }
+    }
+}
