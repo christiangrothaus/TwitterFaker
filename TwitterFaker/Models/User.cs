7c50263daf2d25562f7c53550621981878e7704d
Christian Grothaus <cgrothaus@outlook.com>
2022-10-02 21:36:24 -0400

    Create user model

---
+++ TwitterFaker/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TwitterFaker.Controllers;
+
+namespace TwitterFaker.Models
+{
+    public class User
+    { 
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
