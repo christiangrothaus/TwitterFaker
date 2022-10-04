466517e880cb007ef69647b20a0d4eee990f3400
Christian Grothaus <cgrothaus@outlook.com>
2022-10-03 22:02:59 -0400

    Edit layout to show the login button by default

---
+++ TwitterFaker/Models/TwitterFakerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TwitterFaker.Models
+{
+    public class TwitterFakerContext : IdentityDbContext
+    {
+        public TwitterFakerContext(DbContextOptions<TwitterFakerContext> options)
+            : base(options)
+        { }
+
+        // Table
+        public DbSet<IdentityUser> AspNetUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<IdentityUser>().HasData(
+            //    new IdentityUser
+            //    {
+            //        UserName = "admin",            
+            //    }
+            //);
+        }
+    }
+}
