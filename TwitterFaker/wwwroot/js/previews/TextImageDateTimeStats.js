95dc62050894a2a8df515afceba1c76b11bce6cb
alphapixel1 <alphapixel2@gmail.com>
2022-10-04 03:13:25 -0400

    completed block.cshtml

---
+++ TwitterFaker/wwwroot/js/previews/TextImageDateTimeStats.js
@@ -0,0 +1,93 @@
+﻿class TextImageDateTimeStats {
+    #text; #image; #dateTime; #stats; #elements; #hideButton
+    constructor(text, image, dateTime, stats, elements, hideButton = true) {
+        this.#text = text;
+        this.#image = image;
+        this.#dateTime = dateTime;
+        this.#stats = stats;
+        this.#hideButton = hideButton;
+        this.#elements = elements;
+        this.#updateUIStat();
+    }
+    #updateUIStat() {
+        let e = this.#elements;
+        //Text
+
+        e.textElement.innerHTML = this.#text.replace(/[@#][\w_]+/g, `<span class="simulatedLink">$&</span>`);
+
+        //Image
+        if (this.#image != null) {
+            e.imageContainerElement.classList.remove("hide");
+            e.imageElement.src = this.#image;
+        } else {
+            e.imageContainerElement.classList.add("hide");
+        }
+
+        //Date
+        let dt = this.#dateTime;
+        let currentDate = new Date();
+        let date = `${Months[dt.getMonth()]} ${dt.getUTCDate()}`;
+        if (currentDate.getFullYear() != dt.getFullYear()) {
+            date += `, ${dt.getUTCFullYear()}`;
+        }
+        e.dateElement.innerHTML = date;
+
+        //time
+        if (this.timeElement != null) {
+            this.timeElement.innerHTML = dt.toLocaleTimeString().replace(/:\d+ /g, " ");
+        }
+
+        //Stats
+        for (const i in e.statsElements) {
+            let statElement = e.statsElements[i];
+            let stat = this.#stats[i];
+            if (this.#hideButton) {
+                if (stat > 0) {
+                    statElement.classList.remove("hide");
+                    statElement.querySelector(".tweetNumberBold").innerHTML = TextImageDateTimeStats.addPrefix(stat);
+                    statElement.querySelector(".numberPlural").innerHTML = stat > 1 ? "s" : "";
+                } else {
+                    statElement.classList.add("hide");
+                }
+            } else {
+                if (stat > 0)
+                    statElement.innerHTML = TextImageDateTimeStats.addPrefix(stat);
+                else
+                    statElement.innerHTML = "";
+            }
+        }
+    }
+    static addPrefix(n) {
+        let places = [[1e0, ""], [1e3, 'K'], [1e6, 'M'], [1e9, 'B'], [1e12, 'T']]
+        let lastPlace = places[0];
+        for (const place of places) {
+            if (place[0] > n)
+                return (Math.round(n / (lastPlace[0] / 10)) / 10) + lastPlace[1];
+            else
+                lastPlace = place;
+        }
+    }
+    get text() { return this.#text; }
+    set text(newVal) {
+        this.#text = newVal;
+        this.#updateUIStat();
+    }
+
+    get image() { return this.#image; }
+    set image(newVal) {
+        this.#image = newVal;
+        this.#updateUIStat();
+    }
+
+    get dateTime() { return this.#dateTime; }
+    set dateTime(newVal) {
+        this.#dateTime = newVal;
+        this.#updateUIStat();
+    }
+
+    get stats() { return this.#stats; }
+    set stats(newVal) {
+        this.#stats = newVal;
+        this.#updateUIStat();
+    }
+}
---
+++ TwitterFaker/wwwroot/js/previews/Tweet.js
@@ -0,0 +1,73 @@
+﻿class Tweet extends TwitterElement {
+    #clientElement; #debunkElement;
+    #client; #debunk;
+    constructor(twitterElementJSON, profileJSON, textImageDateTimeStatsJSON, client, debunk) {
+        super(
+            twitterElementJSON.theme,
+            twitterElementJSON.font,
+            document.getElementById("tweetTemplate").content.cloneNode(true).firstElementChild
+        );
+        //let clone=;
+        //this.container=clone.firstElementChild;
+        this.profile = new Profile(
+            {
+                displayNameElement: this.container.querySelector(".displayName"),
+                userNameElement: this.container.querySelector(".userNameText"),
+                profilePicElement: this.container.querySelector(".profileImg"),
+                verifiedElement: this.container.querySelector(".verified"),
+            },
+            profileJSON.displayName,
+            profileJSON.userName,
+            profileJSON.profilePic,
+            profileJSON.verified,
+        );
+        this.TextImageDateTimeStats = new TextImageDateTimeStats(
+            textImageDateTimeStatsJSON.text,
+            textImageDateTimeStatsJSON.image,
+            textImageDateTimeStatsJSON.dateTime,
+            textImageDateTimeStatsJSON.stats,
+            {
+                textElement: this.container.querySelector(".tweetText"),
+                imageElement: this.container.querySelector(".tweetImg"),
+                imageContainerElement: this.container.querySelector(".imgContainer"),
+                dateElement: this.container.querySelector(".detailDate"),
+                timeElement: this.container.querySelector(".detailTime"),
+                statsElements: [
+                    this.container.querySelector(".retweets"),
+                    this.container.querySelector(".quotes"),
+                    this.container.querySelector(".likes")
+                ]
+            }
+        );
+        this.#clientElement = this.container.querySelector(".detailClient");
+        this.#client = client;
+        this.#debunkElement = this.container.querySelector(".debunk");
+        this.#debunk = debunk;
+        this.#updateUITweet();
+    }
+    #updateUITweet() {
+        if (this.#client != null && this.#client.length > 0) {
+            this.#clientElement.classList.remove("hide");
+            this.#clientElement.querySelector(".detailClientText").innerHTML = this.#client;
+        } else {
+            this.#clientElement.classList.add("hide");
+        }
+
+        if (this.#debunk != null && this.#debunk.length > 0) {
+            this.#debunkElement.classList.remove("hide");
+            this.#debunkElement.querySelector(".debunkText").innerHTML = this.#debunk;
+        } else {
+            this.#debunkElement.classList.add("hide");
+        }
+    }
+    get debunk() { return this.#debunk }
+    set debunk(newVal) {
+        this.#debunk = newVal;
+        this.#updateUITweet();
+    }
+    get client() { return this.#client }
+    set client(newVal) {
+        this.#client = newVal;
+        this.#updateUITweet();
+    }
+}
