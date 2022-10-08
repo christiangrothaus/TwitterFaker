1127b690c9e7884502a60b701359c2bc32d59c57
Christian Grothaus <cgrothaus@outlook.com>
2022-10-08 19:56:43 -0400

    Create a PictureConvert service class for handling IFormFile to Base64

---
+++ TwitterFaker/Services/PictureConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace TwitterFaker.Services
+{
+    public class PictureConverter
+    {
+        public static string IFormFileToBase64(IFormFile file)
+        {
+            string result;
+            using (MemoryStream ms = new MemoryStream())
+            { 
+                file.CopyTo(ms);
+                var fileBytes = ms.ToArray();
+                result = Convert.ToBase64String(fileBytes);
+                ms.Close();
+            }
+            return result;
+        }
+    }
+}
