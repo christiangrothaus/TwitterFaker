f67c3b4b1e50bcaab7ec782eaf18c79decd9c131
alphapixel1 <alphapixel2@gmail.com>
2022-10-09 07:53:00 -0400

    added block

---
+++ TwitterFaker/Controllers/BlockController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TwitterFaker.Models;
+using System.Linq;
+using System;
+
+namespace TwitterFaker.Controllers
+{
+    public class BlockController : Controller
+    {
+        private TwitterFakerContext context { get; }
+
+        public BlockController(Models.TwitterFakerContext context)
+        {
+            this.context = context;
+        }
+        // GET: BlocksController
+        public ActionResult Index()
+        {
+            var blocks = context.Blocks.ToList();
+            return View(blocks);
+        }
+
+        // GET: BlocksController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BlocksController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+      /*  // POST: BlocksController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }*/
+
+        [HttpPost]
+        public IActionResult Update(Block block)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                if (block.BlockId == 0)
+                {
+                    //block.BlockId = 0;
+                    context.Blocks.Add(block);
+                }
+                else
+                    context.Blocks.Update(block);
+                context.SaveChanges();
+                //return RedirectToAction("Index", "Block");
+            }
+            return RedirectToAction("Index", "Block");
+        }
+
+        // GET: BlocksController/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Action = "Update";
+            if (id == 0)
+                return View("Edit",null);
+            Console.WriteLine(id);
+            return View("Edit",context.Blocks.First(z=>z.BlockId==id));
+        }
+
+        // POST: BlocksController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BlocksController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit",new Block());
+        }
+
+        // POST: BlocksController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ TwitterFaker/nicksQueries.sql
@@ -0,0 +1,4 @@
+﻿select * from Blocks;
+select * from AspNetUsers;
+delete from Blocks
+insert into Blocks values ('17ade5c7-5347-4c14-b513-b5deeddaede2','ruppygrain','Keshawn Thomas',null,2,1,1,1)
