26777b89a6730cdc273070e6f522170abb39359b
Capy <cgrothaus@outlook.com>
2022-10-10 17:09:21 -0400

    Create Reply Controller and views.  Create form layout for replies.

---
+++ TwitterFaker/Controllers/ReplyController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwitterFaker.Models;
+
+namespace TwitterFaker.Controllers
+{
+    public class ReplyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Create";
+            return View("Edit", new ReplyChain());
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null)
+            {
+            }
+            else
+            {
+            }
+            return View("Edit");
+        }
+
+
+        public IActionResult Update()
+        {
+            return View("Index");
+        }
+    }
+}
