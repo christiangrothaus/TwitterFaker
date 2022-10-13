c8b54966c972e6378a14ab812fcdd3d8eab5d993
Capy <cgrothaus@outlook.com>
2022-10-12 20:31:44 -0400

    Actual working pagination for replychain form

--- TwitterFaker/wwwroot/js/ReplyChainManager.js
+++ TwitterFaker/wwwroot/js/ReplyChainManager.js
@@ -71,7 +71,6 @@ function createPaginationButton(index) {
 }
 
 function switchReply(index) {
-  console.log(index)
   document.querySelector(`#pageList .active`).classList.remove("active")
   document.querySelector(`#page${index}`).classList.add("active")
   showForm(index)
@@ -88,13 +87,41 @@ function showForm(index) {
 }
 
 function deleteReply() {
+  let intialSelectedFormIndex = selectedFormIndex
+  let intialReplyCount = replyCount
   //Selected Form
-  if (selectedFormIndex === replyCount && replyCount !== 2) {
-    document.querySelector(`#reply${replyCount--}`).remove()
-    document.querySelector(`#reply${replyCount}`).classList.remove("visually-hidden")
-    document.querySelector(`#reply${replyCount}`).classList.add("shown")
-    document.querySelector(`#pageList`).lastElementChild.remove();
-    document.querySelector(`#pageList`).lastElementChild.classList.add("active")
-    selectedFormIndex = replyCount
+  if (replyCount > 2) {
+    if (selectedFormIndex === replyCount && replyCount !== 2) {
+      document.querySelector(`#reply${replyCount--}`).remove()
+      document.querySelector(`#reply${replyCount}`).classList.remove("visually-hidden")
+      document.querySelector(`#reply${replyCount}`).classList.add("shown")
+      document.querySelector(`#pageList`).lastElementChild.remove();
+      document.querySelector(`#pageList`).lastElementChild.classList.add("active")
+      selectedFormIndex = replyCount
+    }
+    else {
+      document.querySelector(`#pageList :nth-child(${selectedFormIndex})`).remove();
+      document.querySelector(`#reply${selectedFormIndex++}`).remove()
+      replyCount--
+      document.querySelector(`#reply${selectedFormIndex}`).classList.remove("visually-hidden")
+      document.querySelector(`#reply${selectedFormIndex}`).classList.add("shown")
+      let updateIndex = selectedFormIndex
+      for (updateIndex; updateIndex <= intialReplyCount; updateIndex++) {
+        document.getElementById(`reply${updateIndex}`).id = `reply${intialSelectedFormIndex}`
+        document.getElementById(`inputDisplayName${updateIndex}`).id = `inputDisplayName${intialSelectedFormIndex}`
+        document.getElementById(`inputUserName${updateIndex}`).id = `inputUserName${intialSelectedFormIndex}`
+        document.getElementById(`inputDateTime${updateIndex}`).id = `inputDateTime${intialSelectedFormIndex}`
+        document.getElementById(`inputPfp${updateIndex}`).id = `inputPfp${intialSelectedFormIndex}`
+        document.getElementById(`inputBody${updateIndex}`).id = `inputBody${intialSelectedFormIndex}`
+        document.getElementById(`inputVerified${updateIndex}`).id = `inputVerified${intialSelectedFormIndex}`
+        document.getElementById(`inputRetweets${updateIndex}`).id = `inputRetweets${intialSelectedFormIndex}`
+        document.getElementById(`inputQuoteTweets${updateIndex}`).id = `inputQuoteTweets${intialSelectedFormIndex}`
+        document.getElementById(`inputLikes${updateIndex}`).id = `inputLikes${intialSelectedFormIndex}`
+        document.querySelector(`#pageList #page${updateIndex}`).innerText = `${intialSelectedFormIndex}`
+        document.querySelector(`#pageList #page${updateIndex}`).id = `page${intialSelectedFormIndex}`
+        intialSelectedFormIndex++
+      }
+      document.querySelector(`#pageList #page${--selectedFormIndex}`).classList.add("active")
+    }
   }
 }
