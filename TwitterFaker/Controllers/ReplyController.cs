3e317cf4eb0c6779b588a06d6ca87b0653cda671
Capy <cgrothaus@outlook.com>
2022-10-16 20:00:21 -0400

    Working ReplyChain creation

--- TwitterFaker/Controllers/ReplyController.cs
+++ TwitterFaker/Controllers/ReplyController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 using TwitterFaker.Models;
+using TwitterFaker.Services;
 
 namespace TwitterFaker.Controllers
 {
     public class ReplyController : Controller
     {
+        private readonly ILogger<ReplyController> _logger;
+        private readonly TwitterFakerContext _twitterFakerContext;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public ReplyController(ILogger<ReplyController> logger, TwitterFakerContext twitterFakerContext, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _twitterFakerContext = twitterFakerContext;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,15 +35,37 @@ namespace TwitterFaker.Controllers
             return View("Edit", new ReplyChain());
         }
 
-        public IActionResult Edit(int id)
+        [HttpPost]
+        public async Task<IActionResult> Edit(ReplyChain replyChain, int themeRadios, int fontRadios, IFormCollection formCollection)
         {
-            if (id == null)
-            {
+            IdentityUser user = await _userManager.GetUserAsync(User);
+            if (user == null) {
+                ModelState.AddModelError("NoUser", "Sign in to save the reply chain.");
             }
             else
             {
+                foreach (FormFile picture in formCollection.Files)
+                {
+                    string pictureName = picture.Name;
+                    int index = int.Parse(pictureName[^1].ToString()) - 1;
+                    replyChain.replies[index].ProfilePicture = PictureConverter.IFormFileToBase64(picture);
+                }
+
+                if (replyChain.ReplyChainId == 0)
+                {
+                    replyChain.Theme = themeRadios;
+                    replyChain.Font = fontRadios;
+                    replyChain.User = user;
+                    _twitterFakerContext.ReplyChains.Add(replyChain);
+                    _twitterFakerContext.SaveChanges();
+                }
+                else
+                {
+                    _twitterFakerContext.ReplyChains.Update(replyChain);
+                    _twitterFakerContext.SaveChanges();
+                }
             }
-            return View("Edit");
+            return RedirectToAction("Index");   
         }
 
 
--- TwitterFaker/wwwroot/js/ReplyChainManager.js
+++ TwitterFaker/wwwroot/js/ReplyChainManager.js
@@ -1,127 +1,151 @@
-﻿let replyOne
-let replyTwo
-let replyCount
-let paginationList
-let pageAddListItem
-let pageAddButton
-let replyFormTemplate
-let paginationButtonTemplate
-let pageDeleteButton
-let selectedFormIndex
-
-window.onload = () => {
-  replyFormTemplate = document.getElementById("replyFormTemplate")
-  paginationButtonTemplate = document.getElementById("pageButttonTemplate")
-  pageAddListItem = document.getElementById("pageAddListItem")
-  replyOne = document.getElementById("reply")
-  replyTwo = document.getElementById("reply2")
-  replyFormContainer = document.getElementById("replyForms")
-  selectedFormIndex = 1
-  replyCount = 2
-  paginationList = document.getElementById("pageList")
-  pageAddButton = document.getElementById("addReply")
-  pageDeleteButton = document.getElementById("deleteSelectedReply")
-  //Watches the add reply button and creates a new reply when clicked and switches to the new reply
-  pageAddButton.addEventListener("click", addReply)
-  //Watches the delete reply button and removes the reply form and pagination button. Also shifts replies index if required.
-  pageDeleteButton.addEventListener("click", deleteReply)
-  //Watches the pagination list for click and then switches to the one clicked
-  paginationList.addEventListener("click", function (e) {
-    var target = e.target
-    switchReply(target.textContent)
-  })
-  document.getElementById("defaultTheme").setAttribute("checked", "")
-  document.getElementById("defaultFont").setAttribute("checked", "")
-}
-
-function addReply() {
-  if (replyCount < 8) {
-    replyCount++
-    createReplyForm(replyCount)
-    createPaginationButton(replyCount)
-    let shownForm = document.querySelector("#replyForms .shown")
-    shownForm.classList.add("visually-hidden")
-    shownForm.classList.remove("shown")
-    selectedFormIndex = replyCount
+﻿class Reply {
+  constructor(id, displayName, username, dateTime, profilePicture, body, verified, retweets, quoteTweets, likes, index) {
+    this.id = id
+    this.displayName = displayName
+    this.username = username
+    this.dateTime = dateTime
+    this.profilePicture = profilePicture
+    this.body = body
+    this.verified = verified
+    this.retweets = retweets
+    this.quoteTweets = quoteTweets
+    this.likes = likes
+    this.index = index
   }
+
 }
 
-function createReplyForm(index) {
-  replyForm = document.getElementById("replyFormTemplate").content.cloneNode(true).firstElementChild
-  replyFormContainer.appendChild(replyForm)
-  document.getElementById("treply").id = `reply${index}`
-  document.getElementById("tinputDisplayName").id = `inputDisplayName${index}`
-  document.getElementById("tinputUserName").id = `inputUserName${index}`
-  document.getElementById("tinputDateTime").id = `inputDateTime${index}`
-  document.getElementById("tinputPfp").id = `inputPfp${index}`
-  document.getElementById("tinputBody").id = `inputBody${index}`
-  document.getElementById("tinputVerified").id = `inputVerified${index}`
-  document.getElementById("tinputRetweets").id = `inputRetweets${index}`
-  document.getElementById("tinputQuoteTweets").id = `inputQuoteTweets${index}`
-  document.getElementById("tinputLikes").id = `inputLikes${index}`
-}
+class ReplyPaginationController {
+  constructor(container, formTemplate, pageButtonTemplate, addButton, deleteButton, pageButtons) {
+    this.container = container
+    this.formTemplate = formTemplate
+    this.pageButtonTemplate = pageButtonTemplate
+    this.addButton = addButton
+    this.deleteButton = deleteButton
+    this.pageButtons = pageButtons
+    this.selectedForm = 1
+    this.replyCount = 2
+  }
 
-function createPaginationButton(index) {
-  paginationButton = document.getElementById("pageButtonTemplate").content.cloneNode(true).firstElementChild
-  paginationList.insertBefore(paginationButton, pageAddListItem)
-  document.querySelector("#pageList .active").classList.remove("active")
-  document.getElementById("tpage").innerText = `${index}`
-  document.getElementById("tpage").classList.add("active")
-  document.getElementById("tpage").id = `page${index}`
-}
+  loadReplyChain(replies) {
+    let formIndex = 1;
+    for (i = 0; i < 2; i++) {
+      this.container.querySelector(`#reply${formIndex} #inputDisplayName${formIndex}`).value = replies[i].displayName
+      this.container.querySelector(`#reply${formIndex} #inputDisplayName${formIndex}`).value = replies[i].displayName
+      this.container.querySelector(`#reply${formIndex} #inputDisplayName${formIndex}`).value = replies[i].displayName
 
-function switchReply(index) {
-  document.querySelector(`#pageList .active`).classList.remove("active")
-  document.querySelector(`#page${index}`).classList.add("active")
-  showForm(index)
-  selectedFormIndex = index
-}
 
-function showForm(index) {
-  let shownForm = document.querySelector("#replyForms .shown")
-  shownForm.classList.add("visually-hidden")
-  shownForm.classList.remove("shown")
-  let formToShow = document.querySelector(`#replyForms #reply${index}`)
-  formToShow.classList.remove("visually-hidden")
-  formToShow.classList.add("shown")
-}
-
-function deleteReply() {
-  let intialSelectedFormIndex = selectedFormIndex
-  let intialReplyCount = replyCount
-  //Selected Form
-  if (replyCount > 2) {
-    if (selectedFormIndex === replyCount && replyCount !== 2) {
-      document.querySelector(`#reply${replyCount--}`).remove()
-      document.querySelector(`#reply${replyCount}`).classList.remove("visually-hidden")
-      document.querySelector(`#reply${replyCount}`).classList.add("shown")
-      document.querySelector(`#pageList`).lastElementChild.remove();
-      document.querySelector(`#pageList`).lastElementChild.classList.add("active")
-      selectedFormIndex = replyCount
     }
-    else {
-      document.querySelector(`#pageList :nth-child(${selectedFormIndex})`).remove();
-      document.querySelector(`#reply${selectedFormIndex++}`).remove()
-      replyCount--
-      document.querySelector(`#reply${selectedFormIndex}`).classList.remove("visually-hidden")
-      document.querySelector(`#reply${selectedFormIndex}`).classList.add("shown")
-      let updateIndex = selectedFormIndex
-      for (updateIndex; updateIndex <= intialReplyCount; updateIndex++) {
-        document.getElementById(`reply${updateIndex}`).id = `reply${intialSelectedFormIndex}`
-        document.getElementById(`inputDisplayName${updateIndex}`).id = `inputDisplayName${intialSelectedFormIndex}`
-        document.getElementById(`inputUserName${updateIndex}`).id = `inputUserName${intialSelectedFormIndex}`
-        document.getElementById(`inputDateTime${updateIndex}`).id = `inputDateTime${intialSelectedFormIndex}`
-        document.getElementById(`inputPfp${updateIndex}`).id = `inputPfp${intialSelectedFormIndex}`
-        document.getElementById(`inputBody${updateIndex}`).id = `inputBody${intialSelectedFormIndex}`
-        document.getElementById(`inputVerified${updateIndex}`).id = `inputVerified${intialSelectedFormIndex}`
-        document.getElementById(`inputRetweets${updateIndex}`).id = `inputRetweets${intialSelectedFormIndex}`
-        document.getElementById(`inputQuoteTweets${updateIndex}`).id = `inputQuoteTweets${intialSelectedFormIndex}`
-        document.getElementById(`inputLikes${updateIndex}`).id = `inputLikes${intialSelectedFormIndex}`
-        document.querySelector(`#pageList #page${updateIndex}`).innerText = `${intialSelectedFormIndex}`
-        document.querySelector(`#pageList #page${updateIndex}`).id = `page${intialSelectedFormIndex}`
-        intialSelectedFormIndex++
+    for (i = 2; i < replies.length; i++) {
+
+    }
+  }
+
+  addReply() {
+    if (this.replyCount < 8) {
+      let dbIndex = this.replyCount
+      this.replyCount++
+      // Creates new form
+      let replyForm = this.formTemplate.content.cloneNode(true).firstElementChild
+      this.container.appendChild(replyForm)
+
+      document.getElementById("tinputDisplayName").setAttribute("name", `replies[${dbIndex}].DisplayName`)
+      document.getElementById("tinputUserName").setAttribute("name", `replies[${dbIndex}].UserName`)
+      document.getElementById("tinputDateTime").setAttribute("name", `replies[${dbIndex}].DateTime`)
+      document.getElementById("tinputPfp").setAttribute("name", `profilePicture${dbIndex + 1}`)
+      document.getElementById("tinputBody").setAttribute("name", `replies[${dbIndex}].Body`)
+      document.getElementById("tinputVerified").setAttribute("name", `replies[${dbIndex}].Verified`)
+      document.getElementById("tinputRetweets").setAttribute("name", `replies[${dbIndex}].Retweets`)
+      document.getElementById("tinputQuoteTweets").setAttribute("name", `replies[${dbIndex}].QuoteTweets`)
+      document.getElementById("tinputLikes").setAttribute("name", `replies[${dbIndex}].Likes`)
+      document.getElementById("tinputIndex").setAttribute("name", `replies[${dbIndex}].Index`)
+      document.getElementById("tinputIndex").value = dbIndex
+
+      document.getElementById("treply").id = `reply${this.replyCount}`
+      document.getElementById("tinputDisplayName").id = `inputDisplayName${this.replyCount}`
+      document.getElementById("tinputUserName").id = `inputUserName${this.replyCount}`
+      document.getElementById("tinputDateTime").id = `inputDateTime${this.replyCount}`
+      document.getElementById("tinputPfp").id = `inputPfp${this.replyCount}`
+      document.getElementById("tinputBody").id = `inputBody${this.replyCount}`
+      document.getElementById("tinputVerified").id = `inputVerified${this.replyCount}`
+      document.getElementById("tinputRetweets").id = `inputRetweets${this.replyCount}`
+      document.getElementById("tinputQuoteTweets").id = `inputQuoteTweets${this.replyCount}`
+      document.getElementById("tinputLikes").id = `inputLikes${this.replyCount}`
+      document.getElementById("tinputIndex").id = `inputIndex${this.replyCount}`
+      document.getElementById("tfileClearBtn").id = `fileClearBtn${this.replyCount}`
+      // Creates new page button
+      let paginationButton = this.pageButtonTemplate.content.cloneNode(true).firstElementChild
+      this.pageButtons.appendChild(paginationButton)
+      document.querySelector("#pageList .active").classList.remove("active")
+      document.getElementById("tpage").innerText = `${this.replyCount}`
+      document.getElementById("tpage").classList.add("active")
+      document.getElementById("tpage").id = `page${this.replyCount}`
+      // Shows correct form
+      let shownForm = document.querySelector("#replyForms .shown")
+      shownForm.classList.add("visually-hidden")
+      shownForm.classList.remove("shown")
+      // Updates selected form index to be the reply count
+      this.selectedForm = this.replyCount
+    }
+  }
+
+  deleteReply() {
+    if (this.replyCount > 2) {
+      let formDeleted = parseInt(this.selectedForm)
+      this.replyCount--
+      document.querySelector(`#pageList :nth-child(${formDeleted})`).remove();
+      document.querySelector(`#reply${formDeleted}`).remove()
+      if (this.selectedForm - 1 === this.replyCount) {
+        this.selectReply(this.replyCount)
       }
-      document.querySelector(`#pageList #page${--selectedFormIndex}`).classList.add("active")
+      else {
+        let formToEdit = parseInt(formDeleted) + 1
+        for (formDeleted; formDeleted <= this.replyCount; formDeleted++) {
+          let dbIndex = formDeleted - 1
+          this.container.querySelector(`#inputDisplayName${formToEdit}`).setAttribute("name", `replies[${dbIndex}].DisplayName`)
+          this.container.querySelector(`#inputUserName${formToEdit}`).setAttribute("name", `replies[${dbIndex}].UserName`)
+          this.container.querySelector(`#inputDateTime${formToEdit}`).setAttribute("name", `replies[${dbIndex}].DateTime`)
+          this.container.querySelector(`#inputPfp${formToEdit}`).setAttribute("name", `profilePicture${dbIndex}`)
+          this.container.querySelector(`#inputBody${formToEdit}`).setAttribute("name", `replies[${dbIndex}].Body`)
+          this.container.querySelector(`#inputVerified${formToEdit}`).setAttribute("name", `replies[${dbIndex}].Verified`)
+          this.container.querySelector(`#inputRetweets${formToEdit}`).setAttribute("name", `replies[${dbIndex}].Retweets`)
+          this.container.querySelector(`#inputQuoteTweets${formToEdit}`).setAttribute("name", `replies[${dbIndex}].QuoteTweets`)
+          this.container.querySelector(`#inputLikes${formToEdit}`).setAttribute("name", `replies[${dbIndex}].Likes`)
+          this.container.querySelector(`#inputIndex${formToEdit}`).setAttribute("name", `replies[${dbIndex}].Index`)
+          this.container.querySelector(`#inputIndex${formToEdit}`).value = dbIndex
+
+          this.container.querySelector(`#reply${formToEdit}`).id = `reply${formDeleted}`
+          this.container.querySelector(`#inputDisplayName${formToEdit}`).id = `inputDisplayName${formDeleted}`
+          this.container.querySelector(`#inputUserName${formToEdit}`).id = `inputUserName${formDeleted}`
+          this.container.querySelector(`#inputDateTime${formToEdit}`).id = `inputDateTime${formDeleted}`
+          this.container.querySelector(`#inputPfp${formToEdit}`).id = `inputPfp${formDeleted}`
+          this.container.querySelector(`#inputBody${formToEdit}`).id = `inputBody${formDeleted}`
+          this.container.querySelector(`#inputVerified${formToEdit}`).id = `inputVerified${formDeleted}`
+          this.container.querySelector(`#inputRetweets${formToEdit}`).id = `inputRetweets${formDeleted}`
+          this.container.querySelector(`#inputQuoteTweets${formToEdit}`).id = `inputQuoteTweets${formDeleted}`
+          this.container.querySelector(`#inputLikes${formToEdit}`).id = `inputLikes${formDeleted}`
+          this.container.querySelector(`#inputIndex${formToEdit}`).id = `inputIndex${formDeleted}`
+          this.container.querySelector(`#fileClearBtn${formToEdit}`).id = `fileClearBtn${formDeleted}`
+          this.pageButtons.querySelector(`#page${formToEdit}`).innerText = `${formDeleted}`
+          this.pageButtons.querySelector(`#page${formToEdit}`).id = `page${formDeleted}`
+          formToEdit++
+        }
+        this.selectReply(this.selectedForm)
+      }
     }
+  }
+
+  selectReply(index) {
+    if (document.querySelector("#replyForms .shown") !== null) {
+      document.querySelector(`#pageList .active`).classList.remove("active")
+      let shownForm = document.querySelector("#replyForms .shown")
+      shownForm.classList.add("visually-hidden")
+      shownForm.classList.remove("shown")
+    }
+    document.querySelector(`#page${index}`).classList.add("active")
+    let formToShow = document.querySelector(`#replyForms #reply${index}`)
+    formToShow.classList.remove("visually-hidden")
+    formToShow.classList.add("shown")
+    this.selectedForm = index
   }
 }
