b158dbe63e525981ded35cff421c87dd249ccfe3
Capy <cgrothaus@outlook.com>
2022-10-18 00:39:33 -0400

    Number formatter for twitter likes, retweets, etc

---
+++ TwitterFaker/Services/NumberFormat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TwitterFaker.Services
+{
+    public static class NumberFormat
+    {
+        public static string KiloFormat(this int num)
+        {
+            if (num >= 100000000)
+            { 
+                return (num / 1000000f).ToString("#,0M");
+            }
+
+            if (num >= 10000000)
+            { 
+                return (num / 1000000f).ToString("0.#") + "M";
+            }
+
+            if (num >= 100000)
+            { 
+                return (num / 1000f).ToString("#,0K");
+            }
+
+            if (num >= 10000) 
+            { 
+                return (num / 1000f).ToString("0.#") + "K";
+            }
+            return num.ToString("#,0");
+        }
+    }
+}
